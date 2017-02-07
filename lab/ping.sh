f85078153e67d74ee6cb95d37d24138761435fdb
Zanice <znj@case.edu>
2017-02-07 17:43:54 -0500

    Lab work, lab 1

---
+++ lab/ping.sh
@@ -0,0 +1,14 @@
+# try machine 2
+echo " "
+echo ">>> Google DNS"
+echo " "
+host deeplearning02w 8.8.8.8
+ping deeplearning02w 8.8.8.8 -c 1
+echo " "
+echo " "
+echo ">>> Network Recognition"
+echo " "
+host deeplearning02w
+ping deeplearning02w -c 1
+echo " "
+echo " "
