8db3fb0aaee34ff9dc9c885f1889b33930b17b95
Zanice <znj@case.edu>
2017-02-20 16:15:43 -0500

    Expansion on client, server functionality

--- PS4/path_action_server/src/path_action_client.cpp
+++ PS4/path_action_server/src/path_action_client.cpp
@@ -6,6 +6,26 @@
 #include <actionlib/client/simple_action_client.h>
 #include <path_action_server/pathAction.h>
 
+// path points, in (x, y) pairs
+const double PATH_POINTS[] = {
+	3.0, 0.0,
+	7.0, 4.0,
+	7.0, 5.25,
+	1.9, 5.25,
+	0.15, 7.0,
+	0.15, 12.0
+};
+
+// the length of the path points array
+const int PATH_POINTS_SIZE = sizeof(PATH_POINTS) / sizeof(PATH_POINTS[0]);
+
+// helper method references
+geometry_msgs::Quaternion planarToQuaternion(double phi);
+geometry_msgs::Quaternion getIdentityQuaternion();
+geometry_msgs::PoseStamped createPose(double x, double y);
+geometry_msgs::PoseStamped createNextPose(geometry_msgs::PoseStamped* old_pose, double x, double y);
+void addPoseToPath(double x, double y, geometry_msgs::PoseStamped* pose, path_action_server::pathGoal* path, bool consider_previous);
+
 void onGoalStart() {
 	;
 }
@@ -19,13 +39,106 @@ void onGoalCompletion(const actionlib::SimpleClientGoalState& state, const path_
 }
 
 int main(int argc, char** argv) {
+	// initialize ROS and the client connection
 	ros::init(argc, argv, "path_action_client");
-	
-	path_action_server::pathGoal goal;
-	
 	actionlib::SimpleActionClient<path_action_server::pathAction> client("path_action", true);
 	
-	// try to reach server
+	// wait for a verified server to connect to
+	bool wait_message_shown = false;
+	while (!client.waitForServer(ros::Duration(1.0))) {
+		if (!wait_message_shown) {
+			ROS_INFO("Waiting for connection to action server...");
+			wait_message_shown = true;
+		}
+	}
+	ROS_INFO("Connection to action server established.");
 	
-	//client.sendGoal(goal, &onGoalCompletion, &onGoalStart, &onGoalFeedback);
+	// declare request variables
+	geometry_msgs::PoseStamped pose;
+	path_action_server::pathGoal goal;
+	
+	// start with initial pose
+	addPoseToPath(0.0, 0.0, &pose, &goal, false);
+	
+	// build the path from desired coordinates
+	int pair_index;
+	double x;
+	double y;
+	for (pair_index = 0; pair_index < PATH_POINTS_SIZE - 1; pair_index += 2) {
+		x = PATH_POINTS[pair_index];
+		y = PATH_POINTS[pair_index + 1];
+		ROS_INFO("Creating pose at point %f, %f.", x, y);
+		addPoseToPath(x, y, &pose, &goal, true);
+	}
+	
+	// send the request to the service
+	client.sendGoal(goal, &onGoalCompletion, &onGoalStart, &onGoalFeedback);
+}
+
+// - - - - - - - - - - - - - - - - - - - - -
+// PATH CONSTRUCTION AND MATH HELPER METHODS
+// - - - - - - - - - - - - - - - - - - - - -
+
+// converts a planar phi angle to a quaternion orientation
+geometry_msgs::Quaternion planarToQuaternion(double phi) {
+	geometry_msgs::Quaternion q;
+	q.x = 0.0;
+	q.y = 0.0;
+	q.z = sin(phi / 2);
+	q.w = cos(phi / 2);
+	
+	return q;
+}
+
+// returns the identity quaternion ({0.0, 0.0, 0.0, 1.0})
+geometry_msgs::Quaternion getIdentityQuaternion() {
+	return planarToQuaternion(0.0);
+}
+
+// create a pose from the given (x, y) pair
+geometry_msgs::PoseStamped createPose(double x, double y) {
+	geometry_msgs::Pose p;
+	p.position.x = x;
+	p.position.y = y;
+	p.position.z = 0.0;
+	p.orientation = getIdentityQuaternion();
+	
+	geometry_msgs::PoseStamped ps;
+	ps.pose = p;
+	
+	return ps;
+}
+
+// create a pose from the given (x, y) pair, as a "next step" from a given previous pose
+geometry_msgs::PoseStamped createNextPose(geometry_msgs::PoseStamped* old_pose, double x, double y) {
+	double old_x = (*old_pose).pose.position.x;
+	double old_y = (*old_pose).pose.position.y;
+	double old_z = (*old_pose).pose.position.z;
+	
+	double dx = x - old_x;
+	double dy = y - old_y;
+	double phi = atan2(dy, dx);
+	
+	geometry_msgs::Pose p;
+	p.position.x = x;
+	p.position.y = y;
+	p.position.z = old_z;
+	p.orientation = planarToQuaternion(phi);
+	
+	geometry_msgs::PoseStamped ps;
+	ps.pose = p;
+	
+	return ps;
+}
+
+// create a pose for the (x, y) pair - dependent on if the previous pose should be considered - and add the new pose to the path in construction
+void addPoseToPath(double x, double y, geometry_msgs::PoseStamped* pose, path_action_server::pathGoal* path, bool consider_previous) {
+	if (consider_previous) {
+		*pose = createNextPose(pose, x, y);
+	}
+	else {
+		*pose = createPose(x, y);
+	}
+	
+	(*path).nav_path.poses.push_back(*pose);
 }
--- PS4/path_action_server/src/path_action_server.cpp
+++ PS4/path_action_server/src/path_action_server.cpp
@@ -5,7 +5,53 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <path_action_server/pathAction.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <math.h>
 
+// robot warm-up time
+const double WARM_UP_TIME = 0.2;
+// robot command transition time
+const double TRANS_TIME = 0.1;
+// robot movement speed value in m/s
+const double MOVE_SPEED = 1.0;
+// robot turn speed value in rads/s
+const double TURN_SPEED = 0.5;
+
+// update rate
+const double DT = 0.01;
+
+// minimum number of command iterations that would make an action worthwhile
+const double THRESHOLD_ITERS = 1;
+// threshold for movement adjustment, in m
+const double MOVE_THRESHOLD = MOVE_SPEED * DT * THRESHOLD_ITERS;
+// threshold for turn adjustment, in rads
+const double TURN_THRESHOLD = TURN_SPEED * DT * THRESHOLD_ITERS;
+
+// current pose information variables
+double current_x = 0.0;
+double current_y = 0.0;
+double current_phi = 0.0;
+
+//action publishing variables
+ros::Publisher* twist_commander;
+geometry_msgs::Twist twist_cmd;
+ros::Rate* loop_timer;
+
+// helper method references
+void perform_twist_action(double duration, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+void be_stationary(double duration, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+void move_forward(double distance, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+void make_turn(int direction, double radians, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+void make_turn(double radians, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+void make_right_angle_turn(int direction, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer);
+double quaternionToPlanar(geometry_msgs::Quaternion quaternion);
+double getDistanceBetween(double x1, double y1, double x2, double y2);
+double getDeltaPhi(double phi, double reference);
+
+// class encompassing server object, functionality
 class PathActionServer {
 	private:
 		ros::NodeHandle n_;
@@ -25,12 +71,62 @@ class PathActionServer {
 		void executeGoal(const actionlib::SimpleActionServer<path_action_server::pathAction>::GoalConstPtr& goal);
 };
 
+// server class constructor
 PathActionServer::PathActionServer() : sas_(n_, "path_action", boost::bind(&PathActionServer::executeGoal, this, _1), false) {
 	sas_.start();
 }
 
+// receives and executes a goal from the client
 void PathActionServer::executeGoal(const actionlib::SimpleActionServer<path_action_server::pathAction>::GoalConstPtr& goal) {
-	;
+	int pose_count = goal->nav_path.poses.size();
+	ROS_WARN("Received path request of %d poses.", pose_count);
+	
+	int index;
+	double turn_phi;
+	double forward_dist;
+	geometry_msgs::Pose pose;
+	for (index = 0; index < pose_count; index++) {
+		ROS_INFO("Beginning processing of pose %d:", index);
+		pose = goal->nav_path.poses[index].pose;
+		
+		// display goal pose, current pose information
+		ROS_WARN("\t<POSE %d> x=%f y=%f phi=%f as DESIRED", index, pose.position.x, pose.position.y, quaternionToPlanar(pose.orientation));
+		ROS_INFO("\t<POSE %d> x=%f y=%f phi=%f as CURRENT", index, current_x, current_y, current_phi);
+		
+		// determine the turn to perform as a subgoal for the pose
+		turn_phi = getDeltaPhi(quaternionToPlanar(pose.orientation), current_phi);
+		
+		// check proposed turn against threshold for minimum turn
+		if (turn_phi > TURN_THRESHOLD || turn_phi < -TURN_THRESHOLD) {
+			// perform and record the expected result of the turn <ERROR CHECKING?>
+			ROS_INFO("\t<POSE %d> Turning %f radians.", index, turn_phi);
+			//make_turn(turn_phi, twist_commander, &twist_cmd, loop_timer);
+			//be_stationary(TRANS_TIME, twist_commander, &twist_cmd, loop_timer);
+			current_phi = current_phi + turn_phi;
+		}
+		else {
+			ROS_WARN("\t<POSE %d> %f RADIANS FALL BELOW THRESHOLD. NO TURNING.", index, turn_phi);
+		}
+		
+		// determine the forward movement distance as a subgoal for the pose
+		forward_dist = getDistanceBetween(current_x, current_y, pose.position.x, pose.position.y);
+		
+		// check proposed movement against threshold for minimum movement
+		if (forward_dist > MOVE_THRESHOLD) {
+			// perform and record the expected result of the forward movement <ERROR CHECKING?>
+			ROS_INFO("\t<POSE %d> Moving forward %f meters.", index, forward_dist);
+			//move_forward(forward_dist, twist_commander, &twist_cmd, loop_timer);
+			//be_stationary(TRANS_TIME, twist_commander, &twist_cmd, loop_timer);
+			current_x = pose.position.x;
+			current_y = pose.position.y;
+		}
+		else {
+			ROS_WARN("\t<POSE %d> %f METERS FALL BELOW THRESHOLD. NO MOVEMENT.", index, forward_dist);
+		}
+		
+		// report the results of attempting the current pose
+		ROS_WARN("\t<POSE %d> x=%f y=%f phi=%f as RESULT", index, current_x, current_y, current_phi);
+	}
 	
 	// sas_.isPreemptRequested()
 	// if goal has been cancelled
@@ -38,7 +134,7 @@ void PathActionServer::executeGoal(const actionlib::SimpleActionServer<path_acti
 	
 	// sas_.publishFeedback(feedback_);
 	
-	// sas_.setSucceeded(result_);
+	sas_.setSucceeded(result_);
 }
 
 int main(int argc, char** argv) {
@@ -47,5 +143,103 @@ int main(int argc, char** argv) {
 	PathActionServer server;
 	
 	ros::spin();
+}
+
+// - - - - - - - - - - - - - - - - -
+// MOVEMENT AND MATH HELPER METHODS
+// - - - - - - - - - - - - - - - - -
+
+// execute a twist message publish over a certain duration
+void perform_twist_action(double duration, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	double timer = 0.0;
+	while (timer < duration) {
+		(*twist_commander).publish(*twist_cmd);
+		timer += DT;
+		(*loop_timer).sleep();
+	}
+}
+
+// execute a twist command for stationary status, for some duration
+void be_stationary(double duration, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	// set command parameters
+	(*twist_cmd).linear.x = 0.0;
+	(*twist_cmd).linear.y = 0.0;
+	(*twist_cmd).linear.z = 0.0;
+	(*twist_cmd).angular.x = 0.0;
+	(*twist_cmd).angular.y = 0.0;
+	(*twist_cmd).angular.z = 0.0;
+	
+	// perform action
+	perform_twist_action(duration, twist_commander, twist_cmd, loop_timer);
+}
+
+// execute a twist command for moving forward, for some distance in meters
+void move_forward(double distance, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	// set command parameters
+	(*twist_cmd).linear.x = MOVE_SPEED;
+	(*twist_cmd).linear.y = 0.0;
+	(*twist_cmd).linear.z = 0.0;
+	(*twist_cmd).angular.x = 0.0;
+	(*twist_cmd).angular.y = 0.0;
+	(*twist_cmd).angular.z = 0.0;
+	
+	// perform action
+	perform_twist_action(distance / MOVE_SPEED, twist_commander, twist_cmd, loop_timer);
+}
+
+// execute a twist command for turning positively or negatively, for some amount of radians
+void make_turn(int direction, double radians, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	// error checking: snap direction to ceiling/floor
+	if (direction > 1) {
+		direction = 1;
+	}
+	else if (direction < -1) {
+		direction = -1;
+	}
+	
+	// set command parameters
+	(*twist_cmd).linear.x = 0.0;
+	(*twist_cmd).linear.y = 0.0;
+	(*twist_cmd).linear.z = 0.0;
+	(*twist_cmd).angular.x = 0.0;
+	(*twist_cmd).angular.y = 0.0;
+	(*twist_cmd).angular.z = TURN_SPEED * direction;
+	
+	// perform action
+	perform_twist_action(radians / TURN_SPEED, twist_commander, twist_cmd, loop_timer);
+}
+
+// execute a twist command for some amount of positive or negative radians
+void make_turn(double radians, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	if (radians > 0) {
+		make_turn(1, radians, twist_commander, twist_cmd, loop_timer);
+	}
+	else {
+		make_turn(-1, radians * -1, twist_commander, twist_cmd, loop_timer);
+	}
+}
+
+// execute a twist command for turning pi/2 radians positively or negatively
+void make_right_angle_turn(int direction, ros::Publisher* twist_commander, geometry_msgs::Twist* twist_cmd, ros::Rate* loop_timer) {
+	make_turn(direction, M_PI / 2, twist_commander, twist_cmd, loop_timer);
+}
+
+// assuming a quaternion representing z-axis rotation, converts a quaternion orientation to a planar angle
+double quaternionToPlanar(geometry_msgs::Quaternion quaternion) {
+	double z = quaternion.z;
+	double w = quaternion.w;
+	double phi = 2 * atan2(z, w);
+	
+	return phi;
+}
+
+// finds the distance between two points
+double getDistanceBetween(double x1, double y1, double x2, double y2) {
+	return sqrt(pow((x2 - x1), 2.0) + pow((y2 - y1), 2.0));
+}
+
+// finds the change in angle from some reference angle to phi
+double getDeltaPhi(double phi, double reference) {
+	return phi - reference;
 }
 
